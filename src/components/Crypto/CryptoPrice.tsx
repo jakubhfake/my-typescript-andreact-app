3c2873a7e02e2f13c87fb4fe5843b5a5933b097e
Jakub Haliniak <jakubh.fake@gmail.com>
2023-09-26 06:06:13 +0200

    working on CryptoPrice

--- src/components/Crypto/CryptoPrice.tsx
+++ src/components/Crypto/CryptoPrice.tsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
+import {BinanceOneCryptoPairData} from "../../types/crypto-data";
 
 export const CryptoPrice = () => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState<BinanceOneCryptoPairData[] | null>(null);
 
     useEffect( () => {
         (async () => {
             const res = await fetch('https://api2.binance.com/api/v3/ticker/24hr');
             setData(await res.json());
         })();
-    })
+    }, [])
 
     if  (data == null) {
         return <p>Wczytywanie danych...</p>
@@ -16,9 +17,19 @@ export const CryptoPrice = () => {
 
     return <>
         <label>
-            Wybierz parę kryptowalutową:<br/>
+            Wybierz parę kryptowalutową: <br/>
             <select>
-
+                {
+                    data
+                        .map(pair => (
+                            <option
+                                value={pair.symbol}
+                                key={pair.symbol}
+                        >
+                                {pair.symbol}
+                            </option>
+                    ))
+                }
             </select>
         </label>
     </>
